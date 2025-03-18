8b821a804ebb807480453f262b751a9e67107a9a
Cheesy <92267915+pietrosprudente@users.noreply.github.com>
2025-03-18 21:13:41 +0000

    Update RelayScript.cs

--- RelayScript.cs
+++ RelayScript.cs
@@ -1,67 +1,67 @@
 using System.Threading.Tasks;
 using FishNet.Managing;
-using Unity.Networking.Transport.Relay;
 using Unity.Services.Authentication;
 using Unity.Services.Core;
 using Unity.Services.Relay;
 using Unity.Services.Relay.Models;
 using UnityEngine;
 
-namespace FishNet.Transporting.UTP {
-public class RelayScript : MonoBehaviour
+namespace FishNet.Transporting.UTP
 {
-    public bool hostOnStart = false;
-
-    void Start()
+    public class RelayScript : MonoBehaviour
     {
-        if (hostOnStart)
+        public bool hostOnStart = false;
+
+        void Start()
         {
-            StartHostWithRelay(5);
+            if (hostOnStart)
+            {
+                StartHostWithRelay(5);
+            }
+
+            if (hostOnStart && NetworkManager.Instances[0].ServerManager.AnyServerStarted())
+            {
+                NetworkManager.Instances[0].ClientManager.StartConnection();
+            }
+        }
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        public static async void ConnectToUnityServices()
+        {
+            await UnityServices.InitializeAsync();
         }
 
-        if (hostOnStart && NetworkManager.Instances[0].ServerManager.AnyServerStarted())
-        {
-            NetworkManager.Instances[0].ClientManager.StartConnection();
-        }
-    }
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public static async void ConnectToUnityServices()
-    {
-        await UnityServices.InitializeAsync();
-    }
 
-
-    public static async Task<string> StartHostWithRelay(int maxConnections = 5, bool supportWebGl = false)
-    {
-        await UnityServices.InitializeAsync();
-        if (!AuthenticationService.Instance.IsSignedIn)
+        public static async Task<string> StartHostWithRelay(int maxConnections = 5, bool supportWebGl = false)
         {
-            await AuthenticationService.Instance.SignInAnonymouslyAsync();
-        }
-        Allocation allocation = await RelayService.Instance.CreateAllocationAsync(maxConnections);
-        string connectionType = supportWebGl ? "wss" : "dtls";
-        NetworkManager.Instances[0].GetComponent<FishyUnityTransport>().SetRelayServerData(allocation.ToRelayServerData(connectionType));
-        NetworkManager.Instances[0].GetComponent<FishyUnityTransport>().UseWebSockets = supportWebGl;
-        var joinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
-        return NetworkManager.Instances[0].ServerManager.StartConnection() ? joinCode : null;
-    }
-
-    public static async Task<bool> StartClientWithRelay(string joinCode, bool supportWebGl = false)
-    {
-        await UnityServices.InitializeAsync();
-        if (!AuthenticationService.Instance.IsSignedIn)
-        {
-            await AuthenticationService.Instance.SignInAnonymouslyAsync();
+            await UnityServices.InitializeAsync();
+            if (!AuthenticationService.Instance.IsSignedIn)
+            {
+                await AuthenticationService.Instance.SignInAnonymouslyAsync();
+            }
+            Allocation allocation = await RelayService.Instance.CreateAllocationAsync(maxConnections);
+            string connectionType = supportWebGl ? "wss" : "dtls";
+            NetworkManager.Instances[0].GetComponent<FishyUnityTransport>().SetRelayServerData(allocation.ToRelayServerData(connectionType));
+            NetworkManager.Instances[0].GetComponent<FishyUnityTransport>().UseWebSockets = supportWebGl;
+            var joinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
+            return NetworkManager.Instances[0].ServerManager.StartConnection() ? joinCode : null;
         }
 
-        var joinAllocation = await RelayService.Instance.JoinAllocationAsync(joinCode: joinCode);
-        string connectionType = supportWebGl ? "wss" : "dtls";
-        NetworkManager.Instances[0].GetComponent<FishyUnityTransport>().SetRelayServerData(joinAllocation.ToRelayServerData(connectionType));
-        return !string.IsNullOrEmpty(joinCode) && NetworkManager.Instances[0].ClientManager.StartConnection();
-    }
+        public static async Task<bool> StartClientWithRelay(string joinCode, bool supportWebGl = false)
+        {
+            await UnityServices.InitializeAsync();
+            if (!AuthenticationService.Instance.IsSignedIn)
+            {
+                await AuthenticationService.Instance.SignInAnonymouslyAsync();
+            }
 
-    // Used for Unity Events
-    public void StartHostWithRelay(int maxConnections = 5) => StartHostWithRelay(maxConnections, false);
-    public void StartClientWithRelay(string joinCode) => StartClientWithRelay(joinCode, false);
-}
+            var joinAllocation = await RelayService.Instance.JoinAllocationAsync(joinCode: joinCode);
+            string connectionType = supportWebGl ? "wss" : "dtls";
+            NetworkManager.Instances[0].GetComponent<FishyUnityTransport>().SetRelayServerData(joinAllocation.ToRelayServerData(connectionType));
+            return !string.IsNullOrEmpty(joinCode) && NetworkManager.Instances[0].ClientManager.StartConnection();
+        }
+
+        // Used for Unity Events
+        public void StartHostWithRelay(int maxConnections = 5) => StartHostWithRelay(maxConnections, false);
+        public void StartClientWithRelay(string joinCode) => StartClientWithRelay(joinCode, false);
+    }
 }
